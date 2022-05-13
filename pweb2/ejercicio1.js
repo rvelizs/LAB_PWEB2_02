d9dcc3d8115e71fe7eb20170cdb387b7e45aaea4
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-05-12 22:51:54 -0500

    Crear Ejercicio1.js y base de prueba HTML

---
+++ pweb2/ejercicio1.js
@@ -0,0 +1,17 @@
+function new_Date(d)  {
+	if (d == 0) {
+		return "Domingo";
+	} else if (d == 1) {
+		return "Lunes";
+	} else if (d == 2) {
+		return "Martes";
+	} else if (d == 3) {
+		return "Miércoles";
+	} else if (d == 4) {
+		return "Jueves";
+	} else if (d == 5) {
+		return "Viernes";
+	} else if (d == 6) {
+		return "Sábado";
+	}
+}
