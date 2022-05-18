d0b234df0cae5be9a925dc2538e8f93b9ef29953
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-05-18 00:09:52 -0500

    ejercicio3.js creado

---
+++ pweb2/LAB02/ejercicio3.js
@@ -0,0 +1,9 @@
+let datetoday = new Date();
+
+var day1 = new Date("08/25/2020");
+
+var difference = day1.getTime()-datetoday.getTime();
+
+
+console.log(difference);
+//document.write(difference);
