8f447e46b84bd4b204ba37f933e1f12414e72a6a
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-05-18 02:47:45 -0500

    ejercicio4.js arreglado, extrae bien los -

--- pweb2/LAB02/ejercicio4.js
+++ pweb2/LAB02/ejercicio4.js
@@ -1,11 +1,12 @@
 let url = "https://meet.google.com/khp-pozo-vos";
 
 // Usando substring
-let newUrl1 = url.substring(27, 28);
-let parte2 = url.substring(32, 33);
+let newUrl1 = url.substring(0, 27);
+let parte2 = url.substring(28, 32);
+let parte3 = url.substring(33)
 
 
 // Usando replace
 let newUrl2 = url.replace(/-/g, " ");
 
-console.log(newUrl1 + parte2);
+console.log(newUrl1 + parte2 + parte3);
