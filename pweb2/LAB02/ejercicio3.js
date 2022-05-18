9db2891aec0b7b900860bb6833682dbe87cdfd55
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-05-18 01:22:48 -0500

    ejercicio3.js calculando equivalencias de tiempo

--- pweb2/LAB02/ejercicio3.js
+++ pweb2/LAB02/ejercicio3.js
@@ -1,9 +1,10 @@
-let datetoday = new Date();
+var hoy = new Date();
 
-var day1 = new Date("08/25/2020");
+var dAqp = new Date("15/08/2022");
 
-var difference = day1.getTime()-datetoday.getTime();
+// Con .getTime obtengo el número de milisegundos
 
+var porMin = 1000 * 60; // 1 000 milisegundos en un segundo por 60 segundos (1 minuto)
+var porHor = porMin * 60; // 60 minutos (1 hora)
+var porD = porHor * 24; // 24 horas (1 día)
 
-console.log(difference);
-//document.write(difference);
