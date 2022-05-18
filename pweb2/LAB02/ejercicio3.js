2197682706358ec768cfd7695eee8dac199644e5
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-05-18 02:05:04 -0500

    ejercicio3.js convertido a función

--- pweb2/LAB02/ejercicio3.js
+++ pweb2/LAB02/ejercicio3.js
@@ -1,17 +1,21 @@
-// Fecha actual
-var hoy = new Date();
+function calcDiasAqp(){
+	
+	// Fecha actual
+	var hoy = new Date();
 
-// Día de Arequipa
-var dAqp = new Date("08/15/2022");
+	// Día de Arequipa
+	var dAqp = new Date("08/15/2022");
+	
+	// Con .getTime obtengo el número de milisegundos
+	
+	var porMin = 1000 * 60; // 1 000 milisegundos en un segundo por 60 segundos (1 minuto)
+	var porHor = porMin * 60; // 60 minutos (1 hora)
+	var porD = porHor * 24; // 24 horas (1 día) -> porD es el número de milisegundos en un día
+	
+	let diasAqp = Math.round(dAqp.getTime() / porD);
+	let diasAct = Math.round(hoy.getTime() / porD);
 
-// Con .getTime obtengo el número de milisegundos
+	var numDiasAqp = diasAqp - diasAct;
 
-var porMin = 1000 * 60; // 1 000 milisegundos en un segundo por 60 segundos (1 minuto)
-var porHor = porMin * 60; // 60 minutos (1 hora)
-var porD = porHor * 24; // 24 horas (1 día) -> porD es el número de milisegundos en un día
-
-let diasAqp = Math.round(dAqp.getTime() / porD);
-let diasAct = Math.round(hoy.getTime() / porD);
-
-console.log("Faltan " + (diasAqp - diasAct) + "días");
-
+	document.getElementById("nDias").innerHTML = "Faltan" + numDiasAqp + " días";
+}
