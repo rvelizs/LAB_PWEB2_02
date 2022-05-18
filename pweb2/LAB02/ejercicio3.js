8b0558c625a5a98000e282038a66dd83ae3ced4f
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-05-18 01:34:56 -0500

    ejercicio3.js probando con console.log y enlazar lab02.html con este archivo

--- pweb2/LAB02/ejercicio3.js
+++ pweb2/LAB02/ejercicio3.js
@@ -1,10 +1,15 @@
+// Fecha actual
 var hoy = new Date();
 
+// Día de Arequipa
 var dAqp = new Date("15/08/2022");
 
 // Con .getTime obtengo el número de milisegundos
 
 var porMin = 1000 * 60; // 1 000 milisegundos en un segundo por 60 segundos (1 minuto)
 var porHor = porMin * 60; // 60 minutos (1 hora)
-var porD = porHor * 24; // 24 horas (1 día)
+var porD = porHor * 24; // 24 horas (1 día) -> porD es el número de milisegundos en un día
+
+var diasAqp = Math.round(dAqp.getTime() / porD);
+console.log(diasAqp);
 
