9e904d799633751125c8e69e944ab7b5012c50da
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-05-18 03:12:22 -0500

    Ejericicio4.js editado para recibir dos URL

--- pweb2/LAB02/ejercicio4.js
+++ pweb2/LAB02/ejercicio4.js
@@ -1,17 +1,18 @@
 function quitarG(){
 
 	// Texto
-	let url = document.getElementById("url").value;
+	let url1 = document.getElementById("url1").value;
+	let url2 = document.getElementById("url2").value;
 
 	// Usando substring
-	let parte1 = url.substring(0, 27);
-	let parte2 = url.substring(28, 32);
-	let parte3 = url.substring(33);
+	let parte1 = url1.substring(0, 27);
+	let parte2 = url1.substring(28, 32);
+	let parte3 = url1.substring(33);
 
 	let resultado1 = parte1 + " " + parte2 + " " + parte3;
 
 	// Usando replace
-	let reusltado2 = url.replace(/-/g, " ");
+	let reusltado2 = url2.replace(/-/g, " ");
 
 	document.getElementById("sinG1").innerHTML = "La nueva cadena de URL es : " + resultado1;
 	document.getElementById("sinG2").innerHTML = "La nueva cadena de URL es : " + reusltado2;
