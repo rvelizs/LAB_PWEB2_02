9ca360bd38dc291d22b8ea51e5193fb134cdcdac
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-05-19 00:37:59 -0500

    ejercicio5.js pruebas exitosas de dimensión

--- pweb2/LAB02/ejercicio5.js
+++ pweb2/LAB02/ejercicio5.js
@@ -1,11 +1,12 @@
 function generarDimensiones() {
 	
+	var numElementos = document.getElementById("n").value;
 	// Para crear la distribución
 	fil = Math.floor(Math.random() * numElementos) + 1;
 	col = 0;
 	// *
 	if(numElementos % fil != 0) {
-		col = numElementos / fil + 1;
+		col = Math.floor(numElementos / fil) + 1;
 	}
 	else {
 		col = numElementos / fil;
